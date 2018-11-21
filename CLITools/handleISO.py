c5e8041bc0aa83b9300add4f6f91aa8d3c905fa1
Niklas Aßelmann <n_asse04@uni-muenster.de>
2018-11-21 11:55:04 +0100

    ISO Methode and Testdata

--- CLITools/handleISO.py
+++ CLITools/handleISO.py
@@ -1,12 +1,37 @@
+import igraph as ig
 import helpfunctions as hf
 
 #gets called when the argument of the command request is a ISOxxx
-def extractMetadata(fileFormat, filePath, whatMetadata):
-    metadata = {}
-    # Example how to use object:
-    #metadata["bbox"] = [coor0, coor1, coor2, coor3]
-    metadata["filename"] = filePath[filePath.rfind("/")+1:filePath.rfind(".")]
+def extractMetadata(filePath, whatMetadata):
+        if whatMetadata=="s":
+            metadaten = {}
+            metadaten["Spatial Extent"] = extractSpatialExtentFromGML(filePath)
+            return metadaten
+        if whatMetadata=="t":
+            metadaten = {}
+            metadaten["Temporal Extent"] = extractTemporalExtentFromGML(filePath)
+            return metadaten
+        if whatMetadata=="e":
+            metadaten = {}
+            metadaten["BoundingBox"] = extractSpatialExtentFromGML(filePath)
+            metadaten["StartTime-EndTime"] = extractTemporalExtentFromGML(filePath)
+            metadaten["Shapetypes"] = extractShapeTypeFromGML(filePath)
+            return metadaten
+        else:
+            metadaten = {}
+            return metadaten
 
-    # TO DO
-    
-    return metadata
+def extractSpatialExtentFromGML(filePath):
+    with open(filePath) as gml_file:
+        g = {}
+        Graph = ig.Graph.Read_GML(gml_file)
+        g["Graph"] = Graph
+        return g
+
+def extractTemporalExtentFromGML(filePath):
+    #todo
+    return ""
+
+def extractShapeTypeFromGML(filePath):
+    #todo
+    return ""
